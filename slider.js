5668d3656095f02cfdbc2b4d3098b95d8e56ee33
khcbabu <khaledchowdhury@Khaled-Chowdhurys-MacBook-Air.local>
2012-04-11 14:49:24 +0300

    image animation added

---
+++ slider.js
@@ -0,0 +1,14 @@
+(document).ready(function() {
+	("#slide1-1").click(function() {
+		("#slide1_images").css("left","0");
+	});
+	("#slide1-2").click(function() {
+		("#slide1_images").css("left","-450px");
+	});
+	("#slide1-3").click(function() {
+		("#slide1_images").css("left","-900px");
+	});
+	("#slide1-4").click(function() {
+		("#slide1_images").css("left","-1350px");
+	});
+});
