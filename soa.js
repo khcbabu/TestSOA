b1cdca282313d09d2d3a92d18454fc33302b01fc
unknown <khaled@khaled-PC.(none)>
2012-05-25 10:44:10 +0300

    sidebar updated

--- soa.js
+++ soa.js
@@ -12,3 +12,16 @@ function startSearch(){
 		alert(searchText);
 	}
 }
+
+function toggle(showHideDiv, switchImgTag) {
+        var ele = document.getElementById(showHideDiv);
+        var imageEle = document.getElementById(switchImgTag);
+        if(ele.style.display == "block") {
+                ele.style.display = "none";
+		imageEle.innerHTML = '<img src="images/plus.gif">';
+        }
+        else {
+                ele.style.display = "block";
+                imageEle.innerHTML = '<img src="images/minus.gif">';
+        }
+}
